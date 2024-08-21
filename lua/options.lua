db664d43d50884edc0a2316cfcb0f2b34a630f63
ArekkusuDev <arekkusu.dev@outlook.com>
2024-08-20 19:07:27 -0600

    remove redundant config

--- lua/options.lua
+++ lua/options.lua
@@ -2,15 +2,8 @@ require "nvchad.options"
 
 -- add yours here!
 local o = vim.o
-o.cursorlineopt = "number" -- to enable cursorline!
-o.cursorline = true
 o.wrap = false
-o.swapfile = false
 o.backup = false
 o.writebackup = false
 o.relativenumber = true
 o.scrolloff = 8
-
-vim.filetype.add {
-  extension = { ["http"] = "http" },
-}
